9efcb3c409fa9182a618d94f937694c0a2f63989
Pedro Capriles <caprilespe@outlook.com>
2020-02-01 21:32:55 -0300

    Add functions into the app with React

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return(
+  const ponerFilas = () => [
+    <tr>
+      <td>Pedro Capriles</td>
+      <td>caprilespe@outlook.com</td>
+      <td>pedrocapriles.io</td>
+    </tr>,
+    <tr>
+      <td>Platzi</td>
+      <td>platzi@platzi.com</td>
+      <td>platzi.com</td>
+    </tr>
+  ];
+  return (
     <div className="m-100">
-      <table clasName="table">
+      <table className="table">
         <thead>
           <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
           </tr>
         </thead>
         <tbody>
-        <tr>
-            <td>
-              Pedro Capriles
-            </td>
-            <td>
-              caprilespe@outlook.com
-            </td>
-            <td>
-              pedrocapriles.io
-            </td>
-          </tr>
-          <tr>
-            <td>
-              Platzi
-            </td>
-            <td>
-              platzi@platzi.com
-            </td>
-            <td>
-              platzi.com
-            </td>
-          </tr>
+          {ponerFilas()}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default App;
