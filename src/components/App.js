8dca210a8b535b3df6ee79253795da30d755af22
Pedro Capriles <caprilespe@outlook.com>
2020-02-01 22:37:26 -0300

    React Life Cycle

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,41 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      users: []
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    console.log(`${response}`)
+    this.setState({
       users: [
         {
-          name: 'Pedro Capriles',
-          email: 'caprilespe@outlook.com',
-          website: 'pedrocapriles.io'
+          name: "Pedro Capriles",
+          email: "caprilespe@outlook.com",
+          website: "pedrocapriles.io"
         },
         {
-          name: 'Platzi',
-          email: 'platzi@platzi.com',
-          website: 'platzi.com'
-        },
+          name: "Platzi",
+          email: "platzi@platzi.com",
+          website: "platzi.com"
+        }
       ]
-    }
+    });
   }
-  ponerFilas = () => (
-    this.state.users.map((user)=>(
+  ponerFilas = () =>
+    this.state.users.map(user => (
       <tr>
-        <td>
-          {user.name}
-        </td>
-        <td>
-          {user.email}
-        </td>
-        <td>
-          {user.website}
-        </td>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.website}</td>
       </tr>
-    ))
-  );
-  render(){
+    ));
+  render() {
     return (
       <div className="m-100">
         <table className="table">
@@ -44,14 +46,11 @@ class App extends Component {
               <th>Enlace</th>
             </tr>
           </thead>
-          <tbody>
-            {this.ponerFilas()}
-          </tbody>
+          <tbody>{this.ponerFilas()}</tbody>
         </table>
       </div>
     );
-  };
-
-};
+  }
+}
 
 export default App;
