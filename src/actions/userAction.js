0912c8954651dde8fb6a1b874e1f35419567a154
Pedro Capriles <caprilespe@outlook.com>
2020-02-11 23:51:49 -0300

    Try Catch

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import { GET_USERS } from "../types/userType";
 export const getUsers = () => async dispatch => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  dispatch({
-    type: GET_USERS,
-    payload: response.data
-  });
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    dispatch({
+      type: GET_USERS,
+      payload: response.data
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
 };
