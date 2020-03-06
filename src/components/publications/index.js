d6ea436521c290011106cba7fa7d50819a8d3b56
Pedro Capriles <caprilespe@outlook.com>
2020-03-05 22:43:35 -0300

    Avoid Overwriting

--- src/components/publications/index.js
+++ src/components/publications/index.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
+import Spinner from "../general/Spinner";
+import Fatal from "../general/Fatal";
 import * as userAction from "../../actions/userAction";
 import * as publicationAction from "../../actions/publicationAction";
 
@@ -8,17 +10,49 @@ const { getPublicationByUser } = publicationAction;
 
 class Publications extends Component {
   async componentDidMount() {
-    if (! this.props.userReducer.users.length) {
-      await this.props.getUsers();
+    const {
+      getUsers,
+      getPublicationByUser,
+      match: {
+        params: { key }
+      }
+    } = this.props;
+    if (!this.props.userReducer.users.length) {
+      await getUsers();
     }
-    this.props.getPublicationByUser(this.props.match.params.key);
+    if (this.props.userReducer.error) {
+      return;
+    }
+    if (!("publications_key" in this.props.userReducer.users[key])) {
+      getPublicationByUser(key);
+    }
   }
+
+  postUser = () => {
+    const {
+      userReducer,
+      match: {
+        params: { key }
+      }
+    } = this.props;
+    if (userReducer.error) {
+      return <Fatal message={userReducer.error} />;
+    }
+    if (!userReducer.users.length || userReducer.loading) {
+      return <Spinner />;
+    }
+
+    const name = userReducer.users[key].name;
+
+    return <h1>publications of {name}</h1>;
+  };
   render() {
     console.log(this.props);
     return (
       <Fragment>
         <h1>publications</h1>
         <div>{this.props.match.params.key}</div>
+        {this.postUser()}
       </Fragment>
     );
   }
